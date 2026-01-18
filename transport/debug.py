78d1c147754edd37e0567a8030452e43d460c32b
Mihai <67759165+SchelZ@users.noreply.github.com>
2026-01-18 13:47:42 +0200

    Add debug logging to DebugTransport class

--- transport/debug.py
+++ transport/debug.py
@@ -1,11 +1,19 @@
 class DebugTransport:
-    def __init__(self):
+    def __init__(self, debug=True):
         self.queue = []
+        self.debug = debug
+
+    def log(self, msg):
+        if self.debug:
+            print(f"[TRANSPORT] {msg}")
 
     def send_move(self, x1, y1, x2, y2):
         self.queue.append((x1, y1, x2, y2))
+        self.log(f"Send move queued: ({x1},{y1}) -> ({x2},{y2})")
 
     def poll(self):
         if self.queue:
-            return self.queue.pop(0)
+            move = self.queue.pop(0)
+            self.log(f"Polling move: {move}")
+            return move
         return None
